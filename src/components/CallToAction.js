b60288ff517a991e58a59bc4e8bdca7f96d4844e
Justin Lloyd Baker <justinbaker@Justins-MacBook-Pro-2.local>
2025-04-29 10:48:48 -0600

    call to action button

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -1,14 +1,18 @@
-function CallToAction() {
-    return (
-      <section className="hero">
-        <div className="text">
-          <h1>Little Lemon<br />Chicago</h1>
-          <p>We are a family-owned Mediterranean restaurant focused on traditional recipes with a modern twist.</p>
-          <button>Reserve a Table</button>
-        </div>
-        <img src="/images/MarioandAndrianA.jpg" alt="Delicious food" />
-      </section>
-    );
-  }
+import { Link } from 'react-router-dom';
 
-  export default CallToAction;
+function CallToAction() {
+  return (
+    <section className="hero">
+      <div className="text">
+        <h1>Little Lemon<br />Chicago</h1>
+        <p>We are a family-owned Mediterranean restaurant focused on traditional recipes with a modern twist.</p>
+        <Link to="/reservations">
+          <button>Reserve a Table</button>
+        </Link>
+      </div>
+      <img src="/images/MarioandAndrianA.jpg" alt="Delicious food" />
+    </section>
+  );
+}
+
+export default CallToAction;
